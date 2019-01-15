c55173b2dc3ef651410cbf3321c86b8c81c9c090
wulening <wulening@foxmail.com>
2019-01-15 15:20:01 +0800

    hibernate

---
+++ src/com/how2java/pojo/Category.java
@@ -0,0 +1,21 @@
+package com.how2java.pojo;
+
+public class Category {
+    int id;
+    String name;
+    public int getId(){
+        return id;
+    }
+
+    public void setId(){
+        this.id=id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(){
+        this.name=name;
+    }
+}
